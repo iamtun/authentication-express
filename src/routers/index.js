803918e5edbd14464382a3ffbd0ae54426a1deda
iamtun <letuan19431791.iuh@gmail.com>
2024-05-05 21:50:05 +0700

    add user controller for router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
 import accountRouter from "./account.router.js";
-
+import userRouter from "./user.router.js";
 const rootRouter = Router();
 
-rootRouter.use('/auth', accountRouter)
+rootRouter.use('/auth', accountRouter);
+rootRouter.use('/users', userRouter);
 
 export default rootRouter;
